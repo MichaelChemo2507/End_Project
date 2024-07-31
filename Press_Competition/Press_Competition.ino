2e622f8b5ce3703108c69be1ea44a6cd5fffa85e
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2024-07-31 21:29:20 +0300

    הפעלת פונקציות הגדרת הכפתור בהתאם למצב הארדואינו (מצב הצגה או מצב משחק)

--- Press_Competition/Press_Competition.ino
+++ Press_Competition/Press_Competition.ino
@@ -1,6 +1,12 @@
 #define DISPLAY_MODE D2
+bool dispalyMode;
 void setup(){
+    pinMode(DISPLAY_MODE, INPUT_PULLUP);
+    dispalyMode = (digitalRead(DISPLAY_MODE)) ? true : false;
+    if(dispalyMode)
+        BTN_setup();
 }
 void loop(){
-
+    if(dispalyMode)
+        BTN_loop();
 }
