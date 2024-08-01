87a3522b334a8dad9d716768b7bf41030396d09d
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2024-08-01 10:09:33 +0300

    הגדרה ראשונית לנורה דרוש הדלקה

---
+++ Press_Competition/T15-LED_Config.ino
@@ -0,0 +1,11 @@
+#define PIN_LED_R D1
+#define PIN_LED_G D2
+#define PIN_LED_B D3
+
+void LED_setup(){
+    pinMode(PIN_LED_R, OUTPUT);
+    pinMode(PIN_LED_G, OUTPUT);
+    pinMode(PIN_LED_B, OUTPUT);
+}
+void LED_loop(){
+}
