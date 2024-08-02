ce670ad81bd7ca5475e7462e5db7df95a5ea54a4
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2024-08-02 17:19:41 +0300

    הפעלת הנורה יצירת מכונת מצבים

--- Press_Competition/T15-LED_Config.ino
+++ Press_Competition/T15-LED_Config.ino
@@ -2,13 +2,36 @@
 #define PIN_LED_G D2
 #define PIN_LED_B D3
 
+#define NEW_RECORD 20
+#define FAILURE 30
+#define REDY_TO_PLAY 40
+
+int playerRollStatus = REDY_TO_PLAY;
 void LED_setup(){
-    pinMode(PIN_LED_R, OUTPUT);
-    pinMode(PIN_LED_G, OUTPUT);
-    pinMode(PIN_LED_B, OUTPUT);
+  pinMode(PIN_LED_R, OUTPUT);
+  pinMode(PIN_LED_G, OUTPUT);
+  pinMode(PIN_LED_B, OUTPUT);
+  Update_status();
 }
 void LED_loop(){
     
+}
+void Update_status(){
+  switch (playerRollStatus)
+  {
+  case NEW_RECORD:
+    LED_On(0,255,255);
+    break;
+  
+  case FAILURE:
+    LED_On(255,191,0);
+    break;
+  
+  case REDY_TO_PLAY:
+    LED_Off();
+    break;
+  
+  }
 }
 void LED_On(int Red, int Green, int Blue) {
   analogWrite(PIN_LED_R, Red);
