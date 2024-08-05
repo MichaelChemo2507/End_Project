1bdfaca184f9e0c46b8feb7caa5102ab411f1a8b
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2024-08-05 18:18:22 +0300

    הוספת פונקציות התמודדות עם בקשות שרת - hendleRoot - handleNotfound

--- Press_Competition/T20-Server_Config.ino
+++ Press_Competition/T20-Server_Config.ino
@@ -26,3 +26,23 @@ void wifi_loop() {
   server.handleClient();
   delay(10);
 }
+void handleRoot() {
+  char HTML[2000] = "";
+  server.send(200, "text/html", HTML);
+}
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+
+  server.send(404, "text/plain", message);
+}
