698ac2f03a97482d32358e05f08a9b6fef100188
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2024-08-05 19:17:04 +0300

    חיבור הגדרות השרת עם תנאי המצבים - דרוש בדיקה

--- Press_Competition/Press_Competition.ino
+++ Press_Competition/Press_Competition.ino
@@ -4,21 +4,21 @@ bool isDispalyMode;
 void setup(){
     Serial.begin(9600);
     pinMode(DISPLAY_MODE, INPUT_PULLUP);
-    isDispalyMode = (digitalRead(DISPLAY_MODE) == HIGH) ? true : false;
+    isDispalyMode = (digitalRead(DISPLAY_MODE) == LOW) ? true : false;
     if(isDispalyMode){
-        
+        wifi_Setup();
     }else{
+        wifiClient_Setup();
+        BTN_setup();
+        LED_setup();
     }
-    wifiClient_Setup();
-    BTN_setup();
-    LED_setup();
 }
 void loop(){
     if(isDispalyMode){
-
+        wifi_loop();
     }
     else{
-    }
         BTN_loop();
         LED_loop();
+    }
 }
